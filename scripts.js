3cbcee42f86409c02adeda53cb2d3b327ba5950c
ZobekPL <tymekcierzan@gmail.com>
2018-02-11 12:00:41 +0100

    Animated scroll down

--- scripts.js
+++ scripts.js
@@ -3,11 +3,21 @@ var scrollDownForMoreHeight = scrollDownForMore.height;
 var header = document.querySelector("header");
 
 window.addEventListener("scroll", function(){
-  if(document.body.scrollTop > 5 || document.scrollingElement.scrollTop > 5){
-    scrollDownForMore.style.display = "none";
+  if(document.scrollingElement.scrollTop > 0){
+    if(!scrollDownForMore.classList.contains("fade-out"))
+      scrollDownForMore.classList.add("fade-out");
+    scrollDownForMore.classList.remove("is-paused");
   } else {
-    console.log(scrollDownForMore.offsetHeight)
     scrollDownForMore.style.top = (header.offsetHeight - scrollDownForMoreHeight)+ "px";
+    scrollDownForMore.classList.remove("fade-out");
+    scrollDownForMore.classList.add("fade-in");
+    setInterval(function(){
+      if(document.scrollingElement.scrollTop == 0){
+        scrollDownForMore.classList.add("is-paused");
+        scrollDownForMore.classList.remove("fade-in");
+        scrollDownForMore.classList.add("fade-out");
+      }
+    }, 1000);
     scrollDownForMore.style.display = null;
   }
 });
