0184e4cac7240ff8ba5778dde1fc1c19ea89e2c1
ZobekPL <tymekcierzan@gmail.com>
2018-02-11 12:02:04 +0100

    scrolldown animated only once

--- scripts.js
+++ scripts.js
@@ -4,20 +4,6 @@ var header = document.querySelector("header");
 
 window.addEventListener("scroll", function(){
   if(document.scrollingElement.scrollTop > 0){
-    if(!scrollDownForMore.classList.contains("fade-out"))
-      scrollDownForMore.classList.add("fade-out");
     scrollDownForMore.classList.remove("is-paused");
-  } else {
-    scrollDownForMore.style.top = (header.offsetHeight - scrollDownForMoreHeight)+ "px";
-    scrollDownForMore.classList.remove("fade-out");
-    scrollDownForMore.classList.add("fade-in");
-    setInterval(function(){
-      if(document.scrollingElement.scrollTop == 0){
-        scrollDownForMore.classList.add("is-paused");
-        scrollDownForMore.classList.remove("fade-in");
-        scrollDownForMore.classList.add("fade-out");
-      }
-    }, 1000);
-    scrollDownForMore.style.display = null;
   }
 });
